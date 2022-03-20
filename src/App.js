48d343eb170a03a03e4b99697dfcf82709673e77
Sharjeel Yunus <sharjeel924@gmail.com>
2022-03-20 18:10:39 +0500

    📦 NEW: Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
 
 const App = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello World</h1>
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+                <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+                <Routes />
+                <Footer />
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './global.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import './global.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, document.getElementById('root')
+);
