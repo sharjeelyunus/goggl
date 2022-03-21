081fa0371e9e6a39c0112c8c51bd8c61160ea9df
Sharjeel Yunus <sharjeel924@gmail.com>
2022-03-21 20:10:46 +0500

    📦 NEW: Routes

---
+++ src/components/Results.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Results = () => {
+  return (
+    <div>
+      <h1>Results</h1>
+    </div>
+  );
+};
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
+import { Results } from './Results';
 
 export const Routes = () => {
-  return (
-    <div>
-      <h1>Routes</h1>
-    </div>
-  );
+  const elements = useRoutes([
+    { path: '/', element: <Navigate to='/search' /> },
+    { path: '/search', element: <Results /> },
+    { path: '/videos', element: <Results /> },
+    { path: '/images', element: <Results /> },
+    { path: '/news', element: <Results /> },
+  ]);
+
+  return elements;
 };
