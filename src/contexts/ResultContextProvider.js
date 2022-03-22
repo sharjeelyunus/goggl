c6c8a70ce82c5f7219bf7b4bb63a90a35230a68f
Sharjeel Yunus <sharjeel924@gmail.com>
2022-03-23 00:53:42 +0500

    📦 NEW: Context Provider

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const getResults = async (type) => {
+        setIsLoading(true);
+
+        const response = await fetch(`${baseUrl}${type}`, {
+            method: "GET",
+            headers: {
+                'X-User-Agent': 'desktop',
+                'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+                'X-RapidAPI-Key': 'ad57dc2bedmsh0e8de5bebd619abp14ccbejsn713bf2b5e4ad'
+            }
+        });
+
+        const data = await response.json();
+
+        setResults(data);
+        setIsLoading(false);
+    }
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+            {children}
+        </ResultContext.Provider>
+    )
+}
+
+export const useResultContext = () => useContext(ResultContext);
